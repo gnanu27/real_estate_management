a158e83595d7b69ff9ebf6d24482e4175de9f295
gnanavelv <gnanavelv27@gmail.com>
2019-07-04 17:09:19 +0530

    final

--- bt_homes/settings.py
+++ bt_homes/settings.py
@@ -145,13 +145,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-# Email alert
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'gnanavelv27@gmail.com'
-EMAIL_HOST_PASSWORD = 'chitraSha'
-EMAIL_USE_TLS = True
 
 try: 
     from .local_settings import *
