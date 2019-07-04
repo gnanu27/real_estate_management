f7846f8719ebc339af27005476b23421359d4707
gnanavelv <gnanavelv27@gmail.com>
2019-07-04 16:49:41 +0530

    local setting

--- bt_homes/settings.py
+++ bt_homes/settings.py
@@ -152,3 +152,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'gnanavelv27@gmail.com'
 EMAIL_HOST_PASSWORD = 'chitraSha'
 EMAIL_USE_TLS = True
+
+try: 
+    from .local_settings import *
+except ImportError:
+    pass
