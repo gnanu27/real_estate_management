e34478068251b8655ab51c0d0ca2535a52898548
gnanavelv <gnanavelv27@gmail.com>
2019-07-04 16:29:13 +0530

    last change

--- bt_homes/settings.py
+++ bt_homes/settings.py
@@ -150,5 +150,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'gnanavelv27@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'chitraSha'
 EMAIL_USE_TLS = True
